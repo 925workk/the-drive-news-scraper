bc2b1d8b509e3b2fd33bb9a09622e189734927fb
Alex Pham <925workk@gmail.com>
2020-01-07 11:50:31 -0700

    finished models

---
+++ models/Note.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+
+const NoteSchema = new Schema({
+  
+    title: {
+      type: String
+    },
+  
+    body: {
+      type: String
+    },
+  
+    created: {
+      type: Date,
+      default: Date.now()
+    }
+      
+  });
+
+// This creates our model from the above schema, using mongoose's model method
+var Note = mongoose.model("Note", NoteSchema);
+
+// Export the Note model
+module.exports = Note;
--- server.js
+++ server.js
@@ -2,4 +2,27 @@ const express = require("express");
 const exphbs  = require("express-handlebars");
 const mongoose = require("mongoose");
 
-const app
+var db = require("./models");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.engine(".hbs", exphbs({ 
+    defaultLayout: "main",
+    extname: ".hbs"
+}));
+app.set("view engine", ".hbs");
+
+db.on("error", function(error) {
+    console.log("Mongoose DB Error: ", error);
+});
+
+const htmlRoutes = require("./routes/html-routes");
+app.use(htmlRoutes);
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
